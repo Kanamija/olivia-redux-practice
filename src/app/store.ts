0299dee7dfee2dae9a0ec184231303e8240c9fd1
Kanami Anderson <kanamianderson@gmail.com>
2026-03-25 11:42:18 -0600

    refactor store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -6,3 +6,12 @@ export const store = configureStore({
         counter: countReducer
     }
 });
+
+import { configureStore } from "@reduxjs/toolkit";
+import countReducer from "../features/slice"
+
+export const store = configureStore({
+    reducer: {
+        counter: countReducer
+    }
+});
