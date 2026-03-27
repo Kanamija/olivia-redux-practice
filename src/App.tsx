68a9c423c970fe98b4506cb3dd35a216f5d52449
Kanami Anderson <kanamianderson@gmail.com>
2026-03-27 13:59:08 -0600

    refactpr slice.ts

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,3 @@ function App() {
 }
 
 export default App;
-
-import "./App.css";
-import Counter from "./features/Counter";
-
-function App() {
-  return (
-    <div>
-      <Counter />
-    </div>
-  );
-}
-
-export default App;
--- src/features/slice.ts
+++ src/features/slice.ts
@@ -20,5 +20,24 @@ const slice = createSlice({
 
 
 
+const slice = createSlice({
+    name: "counter",
+    initialState: { count: 0 },
+    reducers: {
+        increment(state) {
+            state.count += 1;
+        },
+
+        decrement(state) {
+            state.count -= 1;
+        },
+
+        reset(state) {
+            state.count = 0;
+        }
+    }
+})
+
+
 export default slice.reducer
 export const { increment, decrement, reset } = slice.actions;
