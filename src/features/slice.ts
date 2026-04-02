78dd7e8e1f97fa895defe61a40e8bd4e13005b89
Kanami Anderson <kanamianderson@gmail.com>
2026-04-02 14:55:47 -0600

    add new slice in slice.tx

--- src/features/slice.ts
+++ src/features/slice.ts
@@ -20,23 +20,7 @@ const slice = createSlice({
 
 
 
-const slice = createSlice({
-    name: "counter",
-    initialState: { count: 0 },
-    reducers: {
-        increment(state) {
-            state.count += 1;
-        },
 
-        decrement(state) {
-            state.count -= 1;
-        },
-
-        reset(state) {
-            state.count = 0;
-        }
-    }
-})
 
 
 export default slice.reducer
