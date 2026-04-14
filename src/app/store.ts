3e80874a49208c842a9a4bdb12410be780187602
Kanami Anderson <kanamianderson@gmail.com>
2026-04-14 15:48:20 -0600

    update store and getSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -10,11 +10,4 @@ export const store = configureStore({
 
 
 
-import { configureStore } from "@reduxjs/toolkit";
-import countReducer from "../features/slice"
 
-export const store = configureStore({
-    reducer: {
-        counter: countReducer
-    }
-});
--- src/features/slice.ts
+++ src/features/slice.ts
@@ -27,20 +27,3 @@ export default slice.reducer
 export const { increment, decrement, reset } = slice.actions;
 
 
-const slice = createSlice({
-    name: "counter",
-    initialState: { count: 0 },
-    reducers: {
-        increment(state) {
-            state.count += 1;
-        },
-
-        decrement(state) {
-            state.count -= 1;
-        },
-
-        reset(state) {
-            state.count = 0;
-        }
-    }
-})
