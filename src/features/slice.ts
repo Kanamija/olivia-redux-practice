43380fdeaaab170cad5e8bf5238cccf8d7ceb552
Kanami Anderson <kanamianderson@gmail.com>
2026-04-14 09:48:28 -0600

    add new slice

--- src/features/slice.ts
+++ src/features/slice.ts
@@ -25,3 +25,22 @@ const slice = createSlice({
 
 export default slice.reducer
 export const { increment, decrement, reset } = slice.actions;
+
+
+const slice = createSlice({
+    name: "counter",
+    initialState: { count: 0 },
+    reducers: {
+        increment(state) {
+            state.count += 1;
+        },
+
+        decrement(state) {
+            state.count -= 1;
+        },
+
+        reset(state) {
+            state.count = 0;
+        }
+    }
+})
